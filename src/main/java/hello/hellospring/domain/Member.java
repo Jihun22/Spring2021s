8539db15cf682bb97a26cf564e7567bd85fad79e
YANHJIHUN <ik5334@naver.com>
2021-06-09 22:47:52 +0900

    JPA 세팅

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,13 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue (strategy =  GenerationType.IDENTITY)
     private  Long id;
     private  String name;
 
