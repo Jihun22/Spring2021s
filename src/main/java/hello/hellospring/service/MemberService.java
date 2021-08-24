50342fcf6efec3535fb3be13326adb06a4ee0911
YANHJIHUN <ik5334@naver.com>
2021-08-24 16:39:26 +0900

    AOP

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -20,10 +20,20 @@ public class MemberService {
      * 회원가입
      */
     public  Long join(Member member){
-        // 같은 이름이 있는 중복 회원 X
-        validateDuplicateMember(member); //중복회원 검증
-        memberRepository.save(member);
-        return  member.getId();
+
+        long start = System.currentTimeMillis();
+        try {
+            // 같은 이름이 있는 중복 회원 X
+            validateDuplicateMember(member); //중복회원 검증
+            memberRepository.save(member);
+            return  member.getId();
+
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish -start;
+            System.out.println("join =" + timeMs +"ms");
+        }
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -38,7 +48,14 @@ public class MemberService {
      * @return
      */
     public List<Member> findMembers(){
-      return   memberRepository.findAll();
+    long start = System.currentTimeMillis();
+      try {
+          return   memberRepository.findAll();
+      } finally {
+        long finish= System.currentTimeMillis();
+          long timeMs =finish - start;
+          System.out.println("findMembers" +timeMs +"ms");
+      }
     }
     public Optional<Member> findOne (Long memberId) {
         return  memberRepository.findById(memberId);
